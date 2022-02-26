156748cc8053d77602e11697607e1180319cb523
luizfilipe29 <luiz_filipe.29@hotmail.com>
2022-02-26 12:24:47 -0300

    Testando a primeira classe de dominio(categoria)

--- src/main/java/com/luizfilipe/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/luizfilipe/cursomc/resources/CategoriaResource.java
@@ -1,16 +1,29 @@
 package com.luizfilipe.cursomc.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.luizfilipe.cursomc.domain.Categoria;
+
 @RestController
-@RequestMapping(value="/categorias")
+@RequestMapping(value = "/categorias")
 public class CategoriaResource {
 
-	@RequestMapping(method=RequestMethod.GET)
-	public String Lista() {
-		return "REST funcionando";
+	@RequestMapping(method = RequestMethod.GET)
+	public List<Categoria> Lista() {
+
+		Categoria cat1 = new Categoria(1, "Informatica");
+		Categoria cat2 = new Categoria(2, "Escritório");
+
+		List<Categoria> lista = new ArrayList<>();
+		lista.add(cat1);
+		lista.add(cat2);
+
+		return lista;
 	}
-	
+
 }
